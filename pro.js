705a29a3ce44cc12d0938bd1d9f4c540634d847e
Kuroshinibalakumar <kuroshinibalakumar050@gmail.com>
2024-06-13 10:48:07 +0530

    Update pro.js

--- pro.js
+++ pro.js
@@ -7,14 +7,4 @@
 
 
 </script>
- <!-- Ion Icons Js -->
-    <script
-      type="module"
-      src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js"
-    ></script>
-    <script
-      nomodule
-      src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js"
-    ></script>
-    <!-- JS -->
-    <script src="js/script.js"></script>
+
